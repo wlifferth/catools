291d6d55f01e5696e22ee7edc1086421f856193c
Will Lifferth <wlifferth@icloud.com>
2018-03-08 11:57:40 -0500

    Fixed bug on graphing gender breakdown

--- catools.py
+++ catools.py
@@ -5,7 +5,7 @@ import numpy as np
 import scipy.stats as stats
 
 def purchase_frequency(customer_id, graph=True, text=True, bounds=None):
-    customer_visits = Counter(customer_id).values()
+    customer_visits = list(Counter(customer_id).values())
     distribution = Counter(customer_visits)
     repeats = 1 - (distribution[1] / sum(distribution.values()))
     if text:
@@ -42,15 +42,15 @@ def gender_breakdown(names, graph=True, text=True):
     genders = map(d.get_gender, first_names)
     genders = map(compress_gender, genders)
     gender_dict = Counter(genders)
-    
+    total_names = sum(gender_dict.values())
     if text:
-        print("Out of {} total customers:".format(sum(gender_dict.values())))
+        print("Out of {} total customers:".format(total_names))
         for key in gender_dict.keys():
-            print("\t{} cusomters were {}".format(gender_dict[key], key))
+            print("\t{} ({:.2f}%) cusomters were {}".format(gender_dict[key], 100 * gender_dict[key]/total_names, key))
     if graph:
         plt.style.use('ggplot')
         plt.figure(dpi=200)
-        plt.bar(range(len(gender_dict)), list(gender_dict.values()), align='center')
+        plt.bar(range(len(gender_dict)), list(list(gender_dict.values())), align='center')
         plt.xticks(range(len(gender_dict)), gender_dict.keys())
         plt.title("Gender Breakdown of Customers", fontsize=24, fontname='Pier Sans')
         plt.show() 
