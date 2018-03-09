a6b7e5b004934df550eb1cf6b18831045beae084
Will Lifferth <wlifferth@icloud.com>
2018-03-09 14:27:59 -0500

    Exposed gender functions via a GenderEstimator class

--- catools.py
+++ catools.py
@@ -4,7 +4,7 @@ import gender_guesser.detector as gender_detector
 import numpy as np
 import scipy.stats as stats
 
-def purchase_frequency(customer_id, graph=True, text=True, bounds=None):
+def purchase_frequency(customer_id, graph=True, text=True, plot_options={}):
     customer_visits = list(Counter(customer_id).values())
     distribution = Counter(customer_visits)
     repeats = 1 - (distribution[1] / sum(distribution.values()))
@@ -13,24 +13,31 @@ def purchase_frequency(customer_id, graph=True, text=True, bounds=None):
     if graph:
         plt.style.use('ggplot')
         plt.figure(dpi=200)
-        plt.hist(customer_visits, bins=[1,2,3,4,5,6,7,8,9,10])
+        plt.hist(customer_visits, bins=[1,2,3,4,5,6,7,8,9,10], **plot_options)
         plt.title("Number of Orders per Customer", fontsize=24, fontname='Pier Sans')
         plt.ylabel("Frequency")
         plt.xlabel("Number of Orders")
-        if bounds != None:
-            plt.xlim(bounds)
-        plt.show()
 
-def gender_breakdown(names, graph=True, text=True):
-    d = gender_detector.Detector()
-    def get_first_name(x):
+
+class GenderEstimator:
+    def __init__(self, estimator=None):
+        estimator = gender_detector.Detector()
+        self.d = estimator
+
+    def estimate(self, x):
+        fn = self.get_first_name(x)
+        raw_gender = self.d.get_gender(fn)
+        processed_gender = self.compress_gender(raw_gender)
+        return processed_gender
+
+    def get_first_name(self, x):
         try:
             fn = x.split()[0]
             return fn
         except:
             return '-' 
-
-    def compress_gender(x):
+    
+    def compress_gender(self, x):
         if x in ['female', 'mostly_female']:
             return 'Female'
         elif x in ['male', 'mostly_male']:
@@ -38,6 +45,10 @@ def gender_breakdown(names, graph=True, text=True):
         else:
             return 'Andro/Unknown' 
 
+
+
+def gender_breakdown(names, graph=True, text=True, plot_options={}):
+    d = gender_detector.Detector()
     first_names = map(get_first_name, names)
     genders = map(d.get_gender, first_names)
     genders = map(compress_gender, genders)
@@ -50,26 +61,23 @@ def gender_breakdown(names, graph=True, text=True):
     if graph:
         plt.style.use('ggplot')
         plt.figure(dpi=200)
-        plt.bar(range(len(gender_dict)), list(list(gender_dict.values())), align='center')
-        plt.xticks(range(len(gender_dict)), gender_dict.keys())
+        plt.bar(range(len(gender_dict)), [gender_dict['Female'], gender_dict['Male'], gender_dict['Andro/Unknown']], align='center', **plot_options)
+        plt.xticks(range(len(gender_dict)), ['Female', 'Male', 'Andro/Unknown'])
         plt.title("Gender Breakdown of Customers", fontsize=24, fontname='Pier Sans')
-        plt.show() 
+        plt.xlabel("Gender")
 
-def average_purchase(transaction_amounts, graph=True, text=True, bounds=None):
+def average_purchase(transaction_amounts, graph=True, text=True, plot_options={}):
     if text:
-        print(stats.describe(transaction_amounts))
+        print("Your avaerage purchase is ${}.".format(np.mean(transaction_amounts)))
     if graph:
         plt.style.use('ggplot')
         plt.figure(dpi=200)
-        plt.hist(transaction_amounts)
+        plt.hist(transaction_amounts, **plot_options)
         plt.title("Individual Transaction Amounts", fontsize=24, fontname='Pier Sans')
         plt.ylabel("Frequency")
         plt.xlabel("Number of Orders")
-        if bounds != None:
-            plt.xlim(bounds)
-        plt.show()
 
-def lifetime_value(customer_ids, transaction_amounts, graph=True, text=True, bounds=None, **kwargs):
+def lifetime_value(customer_ids, transaction_amounts, graph=True, text=True, plot_options={}):
     totals = {}
     for cust, amount in zip(customer_ids, transaction_amounts):
         if cust not in totals:
@@ -81,10 +89,7 @@ def lifetime_value(customer_ids, transaction_amounts, graph=True, text=True, bou
     if graph:
         plt.style.use('ggplot')
         plt.figure(dpi=200)
-        plt.hist(total_vals, **kwargs)
+        plt.hist(total_vals, **plot_options)
         plt.title("Lifetime Value of Customers (to date)", fontsize=24, fontname='Pier Sans')
         plt.ylabel("Frequency")
         plt.xlabel("Lifetime Value")
-        if bounds != None:
-            plt.xlim(bounds)
-        plt.show()
